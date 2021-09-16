670577cc313019cb3de6f63ad1b06d7b46c794f3
martinasalis <marti.salis20@gmail.com>
2021-09-16 18:32:19 +0200

    Modified graph in page sensor

--- webApp/angular12project/src/app/page-sensor/page-sensor.component.ts
+++ webApp/angular12project/src/app/page-sensor/page-sensor.component.ts
@@ -6,6 +6,7 @@ import {Patient, PatientService} from "../patient.service";
 import {MatDialog} from "@angular/material/dialog";
 import {Sensor} from "../sensor.service";
 import { ChartsModule } from 'ng2-charts';
+import * as moment from "moment";
 
 @Component({
   selector: 'app-page-sensor',
@@ -26,7 +27,12 @@ export class PageSensorComponent implements OnInit {
   chartData: any;
   chartLabels: any;
   chartOptions: any;
-  nums: number[] = [];
+  last_day: number[] = [];
+  last2_day: number[] = [];
+  last3_day: number[] = [];
+  date_last_day: string = '';
+  date_last2_day: string = '';
+  date_last3_day: string = '';
   date: string = '';
 
   constructor(private router: Router, private userService: UserService, private doctorService: DoctorService,
@@ -36,19 +42,32 @@ export class PageSensorComponent implements OnInit {
       this.clickedSensor = this.router.getCurrentNavigation()?.extras.state?.boardData;
       this.index = this.router.getCurrentNavigation()?.extras.state?.index;
 
-      this.clickedSensor.Items.forEach((obj: any, i: number) => {
-        const d = new Date(obj.data_timestamp);
-        this.indices.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
-        this.nums.push(obj.device_data.data[this.index].data);
-        //this.indices.push('Sec: ' + (i+1).toString());
-      });
-      /*
-      const d = new Date(this.clickedSensor.Items[this.clickedSensor.length - 1].data_timestamp);
-      this.date = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + ", " + d.toLocaleDateString();
-      console.log(this.date);
-      console.log(this.nums.length);
-      */
+      let last_date = new Date(this.clickedSensor.Items[this.clickedSensor.Items.length - 1].data_timestamp);
+      let start_last_date = new Date(last_date.getFullYear(), last_date.getMonth(), last_date.getDate()).getTime();
+      let end_last_date = new Date(last_date.getFullYear(), last_date.getMonth(), last_date.getDate(), 23,59,59).getTime();
 
+      for(let i = this.clickedSensor.Items.length-1; i >= 0; i--) {
+
+        if(this.clickedSensor.Items[i].data_timestamp >= start_last_date && this.clickedSensor.Items[i].data_timestamp <= end_last_date) {
+          const d = new Date(this.clickedSensor.Items[i].data_timestamp);
+          this.date_last_day = d.toLocaleDateString();
+          this.indices.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+          this.last_day.push(this.clickedSensor.Items[i].device_data.data[this.index].data);
+
+        }
+        else if(this.clickedSensor.Items[i].data_timestamp >= (start_last_date - 86400000) && this.clickedSensor.Items[i].data_timestamp <= (end_last_date - 86400000)) {
+          const d = new Date(this.clickedSensor.Items[i].data_timestamp);
+          this.date_last2_day = d.toLocaleDateString();
+          this.indices.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+          this.last2_day.push(this.clickedSensor.Items[i].device_data.data[this.index].data);
+        }
+        else if(this.clickedSensor.Items[i].data_timestamp >= (start_last_date - 172800000) && this.clickedSensor.Items[i].data_timestamp <= (end_last_date - 172800000)) {
+          const d = new Date(this.clickedSensor.Items[i].data_timestamp);
+          this.date_last3_day = d.toLocaleDateString();
+          this.indices.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+          this.last3_day.push(this.clickedSensor.Items[i].device_data.data[this.index].data);
+        }
+      }
 
     }
     else {
@@ -71,10 +90,19 @@ export class PageSensorComponent implements OnInit {
 
     this.chartData = [
       {
-        data: this.nums,
-        label: 'Dati sensore'
+        data: this.last_day,
+        label: 'Dati sensore in data: ' + this.date_last_day
+      },
+      {
+        data: this.last2_day,
+        label: 'Dati sensore in data: ' + this.date_last2_day
+      },
+      {
+        data: this.last3_day,
+        label: 'Dati sensore in data: ' + this.date_last3_day
       }
     ];
+
 
     this.chartLabels = this.indices;
 
@@ -83,7 +111,22 @@ export class PageSensorComponent implements OnInit {
       scales: {
         y: {
           beginAtZero: true
-        }
+        },
+        xAxes:[{
+          type: 'time',
+          time: {
+            format: "HH:mm:ss",
+            unit: 'minute',
+            unitStepSize: 10,
+            displayFormats: {
+              'second': 'HH:mm:ss',
+              'minute': 'HH:mm:ss',
+              'hour': 'HH:mm:ss',
+              min: '00:00:00',
+              max: '23:59:59'
+            },
+          }
+        }],
       }
     };
   }
